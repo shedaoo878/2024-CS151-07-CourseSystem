1204bc9b91c1848a6a83d0a85a5a278f6f006269
Henry To <115491088+henry-to@users.noreply.github.com>
2024-10-15 20:58:52 -0700

    Update Campus.java

--- Campus.java
+++ Campus.java
@@ -18,31 +18,52 @@ public class Campus {
     public void addDepartment(Department department) {
         if (!departments.contains(department)) {
             departments.add(department);
+            System.out.println("Department " + department.getName() + " added to campus " + campusName);
+        } else {
+            System.out.println("Department " + department.getName() + " already exists on campus.");
         }
     }
 
     public void removeDepartment(Department department) {
-        departments.remove(department);
+        if (departments.remove(department)) {
+            System.out.println("Department " + department.getName() + " removed from campus " + campusName);
+        } else {
+            System.out.println("Department " + department.getName() + " does not exist on campus.");
+        }
     }
 
     public void addClub(Club club) {
         if (!clubs.contains(club)) {
             clubs.add(club);
+            System.out.println("Club " + club.getClubName() + " added to campus " + campusName);
+        } else {
+            System.out.println("Club " + club.getClubName() + " already exists on campus.");
         }
     }
 
     public void removeClub(Club club) {
-        clubs.remove(club);
+        if (clubs.remove(club)) {
+            System.out.println("Club " + club.getClubName() + " removed from campus " + campusName);
+        } else {
+            System.out.println("Club " + club.getClubName() + " does not exist on campus.");
+        }
     }
 
     public void addCourse(Course course) {
         if (!courses.contains(course)) {
             courses.add(course);
+            System.out.println("Course " + course.getTitle() + " added to campus " + campusName);
+        } else {
+            System.out.println("Course " + course.getTitle() + " already exists on campus.");
         }
     }
 
     public void removeCourse(Course course) {
-        courses.remove(course);
+        if (courses.remove(course)) {
+            System.out.println("Course " + course.getTitle() + " removed from campus " + campusName);
+        } else {
+            System.out.println("Course " + course.getTitle() + " does not exist on campus.");
+        }
     }
 
     public ArrayList<Department> getDepartments() {
@@ -76,7 +97,7 @@ public class Campus {
     public int getStudentCount() {
         int totalStudents = 0;
         for (Course course : courses) {
-            totalStudents += course.getEnrolledStudents().size();
+            totalStudents += course.getCurrentNumStudents(); // Assuming this method exists in Course
         }
         return totalStudents;
     }
@@ -89,7 +110,7 @@ public class Campus {
         return courses.size();
     }
 
-    public ArrayList<Course> getCoursesByDepartment(String department) {
+    public ArrayList<Course> getCoursesByDepartment(Department department) {
         ArrayList<Course> departmentCourses = new ArrayList<>();
         for (Course course : courses) {
             if (course.getDepartment().equals(department)) {
@@ -109,8 +130,25 @@ public class Campus {
         return availableClubs;
     }
 
+    public void listCampusInfo() {
+        System.out.println("Campus: " + campusName + " (Location: " + location + ")");
+        System.out.println("Departments:");
+        for (Department department : departments) {
+            System.out.println(" - " + department.getName());
+        }
+        System.out.println("Clubs:");
+        for (Club club : clubs) {
+            System.out.println(" - " + club.getClubName());
+        }
+        System.out.println("Courses:");
+        for (Course course : courses) {
+            System.out.println(" - " + course.getTitle());
+        }
+        System.out.println("Total Students: " + getStudentCount());
+    }
+
+    @Override
     public String toString() {
-        return "Campus: " + campusName
-                + " (Location: " + location + ")";
+        return "Campus: " + campusName + " (Location: " + location + ")";
     }
 }
