e5e4ee1c213038452c1928ed3a6229a9b96e91e5
Henry To <115491088+henry-to@users.noreply.github.com>
2024-10-18 14:17:43 -0700

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,7 +1,6 @@
-
 import java.util.ArrayList;
 
-public class Department{
+public class Department implements Admin {
     private String depName;
     private ArrayList<Course> coursesInDep;
     private ArrayList<Student> studentsInDep;
@@ -10,29 +9,73 @@ public class Department{
 
     public Department(String depName, Campus campus) {
         this.depName = depName;
-        this.coursesInDep = new ArrayList<Course>();
-        this.studentsInDep = new ArrayList<Student>();
-        this.profsInDep = new ArrayList<Professor>();
+        this.coursesInDep = new ArrayList<>();
+        this.studentsInDep = new ArrayList<>();
+        this.profsInDep = new ArrayList<>();
         this.campus = campus;
     }
 
-    public void addCourse(Course c){
+    public void addCourse(Course c) {
         c.setDepartment(this);
         coursesInDep.add(c);
     }
-    public void addStudent(Student s){
+
+    public void addStudent(Student s) {
         s.setDepartment(this);
         studentsInDep.add(s);
     }
-    public void addProf(Professor p){
+
+    public void addProf(Professor p) {
         p.setDepartment(this);
         profsInDep.add(p);
     }
-    
 
-    public String getName(){
+    public String getName() {
         return this.depName;
     }
 
+    public ArrayList<Course> getCoursesInDep() {
+        return coursesInDep;
+    }
+
+    public ArrayList<Student> getStudentsInDep() {
+        return studentsInDep;
+    }
+
+    public ArrayList<Professor> getProfsInDep() {
+        return profsInDep;
+    }
+
+    @Override
+    public void approveCourse(Course course) {
+        addCourse(course);
+        System.out.println("Course " + course.getTitle() + " has been approved and added to the department.");
+    }
+
+    @Override
+    public void removeStudent(Student student) {
+        if (studentsInDep.remove(student)) {
+            System.out.println("Student " + student.getName() + " has been removed from the department.");
+        } else {
+            System.out.println("Student " + student.getName() + " not found in the department.");
+        }
+    }
+
+    @Override
+    public void viewDepartmentDetails() {
+        System.out.println("Department Name: " + depName);
+        System.out.println("Courses Offered: ");
+        for (Course course : coursesInDep) {
+            System.out.println(" - " + course.getTitle());
+        }
+        System.out.println("Students Enrolled: ");
+        for (Student student : studentsInDep) {
+            System.out.println(" - " + student.getName());
+        }
+        System.out.println("Professors in Department: ");
+        for (Professor prof : profsInDep) {
+            System.out.println(" - " + prof.getName());
+        }
+    }
     
 }
